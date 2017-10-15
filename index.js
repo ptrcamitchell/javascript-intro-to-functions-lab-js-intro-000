f9e72fd8e3db76de11947b4918e5e72f416a5e78
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-15 18:40:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function shout(string){
   return string.toUpperCase()
 }
-
+/*
 function whisper(string){
   return string.toLowerCase()
 }
@@ -9,3 +9,4 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase);
 }
+*/
