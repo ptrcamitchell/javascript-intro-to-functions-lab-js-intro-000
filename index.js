0cca373c251a681e59c7efbfc547315fd26c09f4
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-15 19:27:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-    if (string === string.toLowerCase()) {
-      return "I can\'t hear you!";
-    }
-    if (string === string.toUpperCase()) {
-      return  "YES INDEED!";
-    }
-    if (string === "I love you, Grandma.") {
-      return "I love you, too.";
-    }
-
-}
+  switch (string) {
+    case string.toLowerCase():
+      return "I can\'t hear you!"
+      break;
+    case string.toUpperCase():
+      return "YES INDEED!"
+      break;
+    case "I love you, Grandma.":
+      return "I love you, too."
+      break;
+    default:
+      break;
+  }
+ }    
