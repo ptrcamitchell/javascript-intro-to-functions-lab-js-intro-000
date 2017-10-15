5b8a96ee2afb41fd89e6194363718db5682362c2
ptrcamitchell <ptrcamitchell@gmail.com>
2017-10-15 19:14:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ function logWhisper(string){
 function sayHiToGrandma(string){
     if (string === lowercase) {
       return "I can\'t hear you!"
-    } 
+    }
     if (string === uppercase) {
       return "YES INDEED!"
     }
     if (string === "I love you, Grandma.") {
       return "I love you, too."
-    }  
+    }
 
 }
